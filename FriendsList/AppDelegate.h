2e34e266443d488043fc0e79b73ef430151da3ac
katebutenko <katebutenko@gmail.com>
2012-08-28 23:16:32 +0300

    Initial commit of the empty project.

---
+++ FriendsList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FriendsList
+//
+//  Created by Lion User on 28/08/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FriendsList/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FriendsList
+//
+//  Created by Lion User on 28/08/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
